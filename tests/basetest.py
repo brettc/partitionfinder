82b0cc36206f53ad35666b21dcfd42a57784caa6
Brett Calcott <brett.calcott@gmail.com>
2012-03-22 12:06:04 +1100

    moved to using nose test framework

--- tests/basetest.py
+++ tests/basetest.py
@@ -1,6 +1,6 @@
-import unittest
 import sys
 import os
+import nose
 
 # Magically append the root folder so we can run stuff directly from the tests
 # folder if we want
@@ -13,17 +13,4 @@ CFG_PATH = os.path.join(TEST_PATH, 'cfg')
 PHYML_PATH = os.path.join(TEST_PATH, 'phyml')
 ANALYSIS_PATH = os.path.join(TEST_PATH, 'analysis')
 del here
-
-# Setup the logging. We don't want anything going to the output. Just send it
-# out to a testing.log file in the tests folder
-import logging
-logging.getLogger("").addHandler(
-    logging.FileHandler(
-        os.path.join(TEST_PATH, 'testing.log'),
-        mode='w', encoding=None, delay=False))
-logging.getLogger("").setLevel(logging.INFO)
-
-# Use a base class for all our tests.
-class PartitionFinderTestCase(unittest.TestCase):
-    pass
 
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -3,31 +3,24 @@ import os, shutil
 
 from partfinder import config, analysis_method, reporter
 
-class TestAnalysis(PartitionFinderTestCase):
+def load_cfg_and_run(name):
+    try:
+        pth = os.path.join(ANALYSIS_PATH, name)
+        cfg = config.Configuration()
+        cfg.load_base_path(pth)
+        method = analysis_method.choose_method(cfg.search)
+        rpt = reporter.TextReporter(cfg)
+        meth = method(cfg, rpt, True, False)
+        results = meth.analyse()
+        results.compare(cfg)
+    finally:
+        # Always do this
+        shutil.rmtree(cfg.output_path)
 
-    def load_cfg_and_run(self, name):
-        try:
-            pth = os.path.join(ANALYSIS_PATH, name)
-            cfg = config.Configuration()
-            cfg.load_base_path(pth)
-            method = analysis_method.choose_method(cfg.search)
-            rpt = reporter.TextReporter(cfg)
-            meth = method(cfg, rpt, True, False)
-            results = meth.analyse()
-            results.compare(cfg)
-        finally:
-            # Always do this
-            shutil.rmtree(cfg.output_path)
-
-# Dymanically add all separate files as tests
-# Now we can just add new files
-# See here: http://stackoverflow.com/questions/1193909/pythons-unittest-and-dynamic-creation-of-test-cases
-analysis_dirs = os.listdir(ANALYSIS_PATH)
-for f in analysis_dirs:
-    # if os.path.isdir(f):
-    def ch(f):
-        return lambda self: self.load_cfg_and_run(f)
-    setattr(TestAnalysis, 'test_' + f, ch(f))
+def test_all_analyses():
+    analysis_dirs = os.listdir(ANALYSIS_PATH)
+    for f in analysis_dirs:
+        yield load_cfg_and_run, f
 
 if __name__ == '__main__':
-    unittest.main()
+    nose.runmodule()
