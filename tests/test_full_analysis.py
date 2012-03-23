36fd53595bd5f27905b687886f63c80c8b237a11
Brett Calcott <brett.calcott@gmail.com>
2012-03-23 17:05:58 +1100

    new testing almost done

--- tests/test_full_analysis.py
+++ tests/test_full_analysis.py
@@ -1,17 +1,29 @@
 from basetest import *
-import os, shutil
-
-from partfinder import config, analysis_method, reporter
+import os, shutil, sys
 from zipfile import ZipFile
 
 from nose.plugins.attrib import attr
 from nose.tools import raises
 
+from partfinder import config, analysis_method, reporter
+from partfinder import analysis, scheme, util
+
+
 FULL_PATH = os.path.join(TEST_PATH, 'full_analysis')
+# def path_for(nm):
+    # return os.path.join(FULL_PATH, nm)
+
+# We grab the name of the path from the function name. A bit sneaky, but it
+# avoids screwing up function name / folder correspondence. See here:
+# http://code.activestate.com/recipes/66062-determining-current-function-name/
+def path_from_function():
+    # get name of the caller function (up 1 level in stack frame)
+    funcname = sys._getframe(1).f_code.co_name
+    # Remove 'test_'
+    dirname = funcname[5:]
+    return os.path.join(FULL_PATH, dirname)
 
 def load_cfg_and_run(pth, compare=True, restart=True):
-    # try:
-    # pth = os.path.join(FULL_PATH, name)
     cfg = config.Configuration()
     cfg.load_base_path(pth)
     method = analysis_method.choose_method(cfg.search)
@@ -20,35 +32,176 @@ def load_cfg_and_run(pth, compare=True, restart=True):
     results = meth.analyse()
     if compare:
         results.compare(cfg)
-    # finally:
-        # Always do this
-        # shutil.rmtree(cfg.output_path)
 
-# @attr('slow')
-# def test_DNA1():
-    # load_cfg_and_run('DNA1')
+    # Note -- we only get here if everything completes
+    # Failed tests leave shit around!
+    shutil.rmtree(cfg.output_path)
+    os.remove(os.path.join(cfg.base_path, 'log.txt'))
 
 def load_rerun(pth):
     dna3 = ZipFile(os.path.join(FULL_PATH, 'DNA3-analysis.zip'))
     dna3.extractall(pth)
     load_cfg_and_run(pth, compare=False, restart=False)
-    # Now run it....
 
-# @attr('rerun')
+# See ./full_analysis/tests.txt for details
+# NOTE We could get all of these automatically, but declaring makes it easier
+# to play with individual details
+# DNA -----------------------------------
+
+@attr('slow', 'DNA')
+def test_DNA1():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA2():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA3():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA4():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA5():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA6():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'DNA')
+def test_DNA8():
+    load_cfg_and_run(path_from_function())
+
+# Protein -----------------------------------
+@attr('slow', 'prot')
+def test_prot1():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot2():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot3():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot4():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot5():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot6():
+    load_cfg_and_run(path_from_function())
+
+@attr('slow', 'prot')
+def test_prot8():
+    load_cfg_and_run(path_from_function())
+
+# Re-running ------------------------------
+@attr('rerun')
+def test_rerun01():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun02():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun03():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun04():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun05():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'slow')
+def test_rerun06():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun07():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+def test_rerun08():
+    load_rerun(path_from_function())
+
+# FAILS 
+@attr('rerun', 'fails')
 @raises(config.ConfigurationError)
-def load_rerun_configuration_failure(pth):
-    load_rerun(pth)
+def test_rerun09():
+    load_rerun(path_from_function())
 
-RERUN_SUCCESS = "01 02 03 04 05 06 07 08 17".split()
-RERUN_CONFIGURATION_ERROR = "09 10 11 12 13 14 15 16 17 18 19 20 21".split()
-# RERUN_FAIL = "14 15".split()
+@attr('rerun', 'fails')
+@raises(config.ConfigurationError)
+def test_rerun10():
+    load_rerun(path_from_function())
 
-# @attr('slow')
-# @raises(config.ConfigurationError)
-def test_failed_rerun():
-    for t in RERUN_CONFIGURATION_ERROR:
-        pth = os.path.join(FULL_PATH, 'rerun%s' % t)
-        yield load_failed_rerun, pth
+@attr('rerun', 'fails')
+@raises(config.ConfigurationError)
+def test_rerun11():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'fails')
+@raises(config.ConfigurationError)
+def test_rerun12():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'fails')
+@raises(config.ConfigurationError)
+def test_rerun13():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'fails')
+@raises(analysis.AnalysisError)
+def test_rerun14():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'fails')
+@raises(analysis.AnalysisError)
+def test_rerun15():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'fails')
+@raises(config.ConfigurationError)
+def test_rerun16():
+    load_rerun(path_from_function())
+
+@attr('rerun', 'slow')
+def test_rerun17():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+@raises(util.PartitionFinderError)
+def test_rerun18():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+@raises(util.PartitionFinderError)
+def test_rerun19():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+@raises(util.PartitionFinderError)
+def test_rerun20():
+    load_rerun(path_from_function())
+
+@attr('rerun')
+@raises(util.PartitionFinderError)
+def test_rerun21():
+    load_rerun(path_from_function())
 
 if __name__ == '__main__':
     nose.runmodule()
