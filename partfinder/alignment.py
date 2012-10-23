fe6c4ea6bb3085f97db3428194bc22b5156db055
RobertLanfear <rob.lanfear@gmail.com>
2012-10-23 17:28:31 +1100

    fixed the error catching for data_blocks
    The old zero-indexing problem. This now works. The ONLY place it would
    have failed (which one user eventually spotted) is if you had a
    datablock which specified exactly ONE more site than was in the
    original alignment. Ahhhh well.

--- partfinder/alignment.py
+++ partfinder/alignment.py
@@ -215,7 +215,7 @@ class SubsetAlignment(Alignment):
         Alignment.__init__(self)
 
         #let's do a basic check to make sure that the specified sites aren't > alignment length
-        site_max = max(subset.columns)
+        site_max = max(subset.columns)+1
         log.debug("Max site in data_blocks: %d; max site in alignment: %d" %(site_max, source.sequence_len))
         if site_max>source.sequence_len:
             log.error("Site %d is specified in [data_blocks], but the alignment only has %d sites. Please check." %(site_max, source.sequence_len)) 
