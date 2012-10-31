a87899c1b56b745bec7e6ffbeb455542d5c83802
Rob Lanfear <rob.lanfear@gmail.com>
2012-10-31 14:46:41 +1100

    fixed make_branch_lengths in phyml.py
    this had some legacy code left over from the merge. Now fixed.

--- partfinder/phyml.py
+++ partfinder/phyml.py
@@ -148,7 +148,6 @@ def make_topology(alignment_path, datatype):
 
 def make_branch_lengths(alignment_path, topology_path, datatype):
     # Now we re-estimate branchlengths using a GTR+I+G model on the (unpartitioned) dataset
-    log.info("Estimating GTR+I+G branch lengths on tree")
     dir_path, fname = os.path.split(topology_path)
     tree_path = os.path.join(dir_path, 'topology_tree.phy')
     log.debug("Copying %s to %s", topology_path, tree_path)
@@ -170,24 +169,6 @@ def make_branch_lengths(alignment_path, topology_path, datatype):
 
     # Now return the path of the final tree alignment
     return tree_path
-
-def make_branch_lengths_protein(alignment_path, topology_path):
-    # Now we re-estimate branchlengths using the LG model on the (unpartitioned) dataset
-    log.info("Estimating LG+F branch lengths on tree")
-    dir_path, fname = os.path.split(topology_path)
-    tree_path = os.path.join(dir_path, 'topology_tree.phy')
-    log.debug("Copying %s to %s", topology_path, tree_path)
-    dupfile(topology_path, tree_path)
-
-    command = "-i '%s' -u '%s' -m LG -c 1 -v 0 -f m -d aa -o lr -b 0" % (
-        alignment_path, tree_path)
-    run_phyml(command)
-
-    output_path = make_tree_path(alignment_path)
-    log.info("Branchlength estimation finished")
-
-    # Now return the path of the final tree alignment
-    return output_path
 
 
 def analyse(model, alignment_path, tree_path, branchlengths):
