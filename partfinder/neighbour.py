3a5fac1ed7090c6c9d1d6c90dba1b432b34a11c5
Brett Calcott <brett.calcott@gmail.com>
2012-11-21 14:20:24 -0800

    remove conflicts

--- partfinder/neighbour.py
+++ partfinder/neighbour.py
@@ -11,8 +11,8 @@
 #General Public License for more details. You should have received a copy
 #of the GNU General Public License along with this program.  If not, see
 #<http://www.gnu.org/licenses/>. PartitionFinder also includes the PhyML
-#program, the RAxML program, the PyParsing library, and the python-cluster library 
-#all of which are protected by their own licenses and conditions, using 
+#program, the RAxML program, the PyParsing library, and the python-cluster library
+#all of which are protected by their own licenses and conditions, using
 #PartitionFinder implies that you agree with those licences and conditions as well.
 
 from cluster import minkowski_distance, genmatrix, printmatrix
@@ -32,14 +32,14 @@ def sum_matrices(mat1, mat2, mat3):
 
 def normalise_and_weight(matrix, normaliser, weight):
     """normalise and weight a 2D matrix (list of lists) using the passed value"""
-    
+
     if normaliser==0:
-        #then our matrix must have had a maximum distance of zero anyway, indicating that 
+        #then our matrix must have had a maximum distance of zero anyway, indicating that
         #everything is already identical (e.g. same amino acid model)
         #in this case we don't need to do any work, because these distances won't count
         #when we sum the matrices anyway, so just return without worrying
         return matrix
-        
+
     rowindex=0
     for row in matrix:
         colindex = 0 # keep track of where we are in the matrix
@@ -49,13 +49,13 @@ def normalise_and_weight(matrix, normaliser, weight):
         rowindex += 1
 
     return matrix
-    
-    
+
+
 def get_minmax(matrix):
     """return the min and max distances in a 2D list
     """
 
-    maxdistance=None    
+    maxdistance=None
     mindistance=None
     rowindex=0
     for row in matrix:
@@ -71,16 +71,16 @@ def get_minmax(matrix):
     return mindistance, maxdistance
 
 def get_closest(matrix, subsets):
-    """return the closest subsets defined by a distance matrix 
+    """return the closest subsets defined by a distance matrix
     usually there will just be a pair that's closer than all other pairs
     BUT, it's feasible (if unlikely) that >2 subsets are equally close.
-    This is possible if, e.g. all weights are zero. Then we just want to group all the 
+    This is possible if, e.g. all weights are zero. Then we just want to group all the
     equally close subsets...
-    
+
     So, we return a list of all the closest subsets
     """
     from random import randrange
-    
+
     min_rows = set()
     min_cols = []
     mindistance=None
@@ -90,7 +90,7 @@ def get_closest(matrix, subsets):
         for cell in row:
             #ignore the diagonal
             if (colindex != rowindex):
-                if (cell<mindistance or mindistance is None): 
+                if (cell<mindistance or mindistance is None):
                     mindistance=cell
                     min_rows = set([rowindex])
                 elif cell == mindistance:
@@ -122,7 +122,7 @@ def get_closest_subsets(scheme, weights):
     rates = [] #tree length
     freqs = [] #amino acid or base frequencies
     model = [] #model parameters e.g. A<->C
-    
+
     for s in scheme.subsets:
         param_dict = s.get_param_values()
         subsets.append(s)
@@ -143,7 +143,7 @@ def get_closest_subsets(scheme, weights):
     freqs_matrix = normalise_and_weight(freqs_matrix, max, weights["freqs"])
     min, max = get_minmax(model_matrix)
     model_matrix = normalise_and_weight(model_matrix, max, weights["model"])
-    
+
     #4. sum the matrices
     distance_matrix = sum_matrices(rates_matrix, freqs_matrix, model_matrix)
     #printmatrix(distance_matrix)
@@ -151,22 +151,22 @@ def get_closest_subsets(scheme, weights):
     #5. get the closest pair
     closest_subsets = get_closest(distance_matrix, subsets)
     #print closest_subsets
-    
+
     return closest_subsets
 
 def get_nearest_neighbour_scheme(
         start_scheme, scheme_name, cfg):
     """The idea here is to take a scheme, and perform some analyses to find a neighbouring
-    scheme, where the neighbour has one less subset than the current scheme. 
+    scheme, where the neighbour has one less subset than the current scheme.
     Really this is just progressive clustering, but specified to work well with PartitionFinder
-    
+
     The weights argument allows us to assign different weights to different model parameters
-    
+
     """
-        
+
     import subset
     import scheme
-    
+
     #1. First we get the closest subsets, based on some weights. This will almost always
     #   be two subsets, but it's generalised so that it could be all of them...
     #   cluster weights is a dictionary of weights, keyed by: rate, freqs, model
@@ -178,18 +178,18 @@ def get_nearest_neighbour_scheme(
     for s in closest_subsets:
         newsub_parts = newsub_parts + list(s.partitions)
     newsub = subset.Subset(*tuple(newsub_parts))
-    
+
     #3. Then we define a new scheme with those merged subsets
     all_subs = [s for s in start_scheme.subsets]
 
     #pop out the two subsets we're going to join together
     for s in closest_subsets:
         all_subs.remove(s)
-    
+
     all_subs.append(newsub)
 
     scheme = (scheme.Scheme(cfg, str(scheme_name), *tuple(all_subs)))
 
-    return scheme    
-        
-    
+    return scheme
+
+
