d32a5e974716c8b0bb558bf40f60c9d1f438e39c
Rob Lanfear <rob.lanfear@gmail.com>
2012-12-09 15:44:07 +1100

    new function to return a list of ranked clustered schemes
    entirely untested, off for a bike ride.

--- partfinder/neighbour.py
+++ partfinder/neighbour.py
@@ -70,6 +70,46 @@ def get_minmax(matrix):
 
     return mindistance, maxdistance
 
+def get_ranked_list(matrix, subsets):
+    """return the closest subsets defined by a distance matrix 
+    usually there will just be a pair that's closer than all other pairs
+    BUT, it's feasible (if unlikely) that >2 subsets are equally close.
+    This is possible if, e.g. all weights are zero. Then we just want to group all the 
+    equally close subsets...
+    
+    So, we return a list of all the closest subsets
+    """
+    
+    #let's make a dict keyed by the distance in the matrix, using setdefault to 
+    #add things, in case there are subsets with identical distances
+    distances ={}
+    rowindex=0
+    for row in matrix:
+        colindex=0
+        for cell in row:
+            #ignore the diagonal and sub-diagonal (it's a symmetrical matrix)
+            if (colindex >= rowindex):
+                #get any subs that we already know are that distance apart as a set
+                #default to empty set if it's a new distance                
+                subs = distances.setdefault(cell, ()) 
+                #add subs that correspond to this cell
+                subs.add(subsets[rowindex], subset[colindex])
+            colindex += 1
+        rowindex += 1
+
+    
+    ordered_subsets = []    
+
+    unique_distances = distances.keys()
+    unique_distances.sort()
+    
+    for d in unique_distances:
+        ordered_subsets.append(list(distances[d]))
+
+    return ordered_subsets
+
+
+
 def get_closest(matrix, subsets):
     """return the closest subsets defined by a distance matrix 
     usually there will just be a pair that's closer than all other pairs
@@ -170,8 +210,45 @@ def get_ranked_clustered_schemes(
     We then just return the list of schemes, ordered by closest to most distant in the 
     clustering space
     """
-    pass
+    subsets = [] #a list of subset names, so we know the order things appear in the list
+    for s in scheme.subsets:
+        subsets.append(s)
     
+    distance_matrix = get_disctance_matrix(scheme, cfg.cluster_weights)
+    
+    ranked_subset_groupings = get_ranked_list(distance_matrix, subsets)
+    
+    ranked_clustered_schemes = []
+    for g in ranked_subset_groupings:
+        scheme = make_clustered_scheme(start_scheme, scheme_name, closest_subsets)
+        ranked_clustered_schemes.append(scheme)
+    
+    return scheme
+    
+def make_clustered_scheme(start_scheme, scheme_name, subsets_to_cluster):
+    
+    #1. Create a new subset that merges the subsets_to_cluster
+    newsub_parts = []
+    for s in closest_subsets:
+        newsub_parts = newsub_parts + list(s.partitions)
+    newsub = subset.Subset(*tuple(newsub_parts))
+    
+    #2. Then we define a new scheme with those merged subsets
+    all_subs = [s for s in start_scheme.subsets]
+
+    #pop out the subsets we're going to join together
+    for s in closest_subsets:
+        all_subs.remove(s)
+    
+    #and now we add back in our new subset...
+    all_subs.append(newsub)
+
+    #and finally create the clustered scheme
+    scheme = (scheme.Scheme(cfg, str(scheme_name), *tuple(all_subs)))
+
+    return scheme    
+
+
 
 def get_nearest_neighbour_scheme(
         start_scheme, scheme_name, cfg):
@@ -192,22 +269,8 @@ def get_nearest_neighbour_scheme(
     #   for the overall subset rate, the base/aminoacid frequencies, and the model parameters
     closest_subsets = get_closest_subsets(start_scheme, cfg.cluster_weights)
 
-    #2. Next we create a new subset that merges those two subsets
-    newsub_parts = []
-    for s in closest_subsets:
-        newsub_parts = newsub_parts + list(s.partitions)
-    newsub = subset.Subset(*tuple(newsub_parts))
-    
-    #3. Then we define a new scheme with those merged subsets
-    all_subs = [s for s in start_scheme.subsets]
+    scheme = make_clustered_scheme(start_scheme, scheme_name, closest_subsets)
 
-    #pop out the two subsets we're going to join together
-    for s in closest_subsets:
-        all_subs.remove(s)
-    
-    all_subs.append(newsub)
-
-    scheme = (scheme.Scheme(cfg, str(scheme_name), *tuple(all_subs)))
 
     return scheme    
         
