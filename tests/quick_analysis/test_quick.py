6fb0c5e7c8c8f3c1b1902ac6ea222b14d04d3a22
Brett Calcott <brett.calcott@gmail.com>
2012-12-11 22:01:24 -0500

    Simplify quick tests

--- tests/quick_analysis/test_quick.py
+++ tests/quick_analysis/test_quick.py
@@ -4,23 +4,24 @@ from partfinder import main
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_test_folders():
+    testdirs = []
+    # Get everything that is here...
+    for p in os.listdir(HERE):
+        if p.startswith('__'):
+            continue
+        d = os.path.join(HERE, p)
+        if os.path.isdir(d):
+            testdirs.append(p)
+    return testdirs
+
+
 def pytest_generate_tests(metafunc):
+    # This function feeds the output of the above function into the tests below
     if 'test_folder' in metafunc.fixturenames:
-        testdirs = []
-        for p in os.listdir(HERE):
-            if p.startswith('__'):
-                continue
-            d = os.path.join(HERE, p)
-            if os.path.isdir(d):
-                testdirs.append(p)
-        metafunc.parametrize("test_folder", testdirs)
+        metafunc.parametrize("test_folder", get_test_folders())
 
 
-def test_quick_asss(test_folder):
-    "ntoheu oeu oeu"
+def test_quick_analysis(test_folder):
     full_path = os.path.join(HERE, test_folder)
     main.call_main("DNA", "%s --force-restart" % full_path)
-
-
-
-
