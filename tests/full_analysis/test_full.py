531828b895cae3d85e5c5eeeb86b593ddbe70c50
Brett Calcott <brett.calcott@gmail.com>
2013-04-02 16:45:58 +1100

    Simplify paramaterizing test usage

--- tests/full_analysis/test_full.py
+++ tests/full_analysis/test_full.py
@@ -5,37 +5,30 @@ from zipfile import ZipFile
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
+organise_tests = {
+    "dna"               : ["DNA%d" % n for n in range(1, 9)],
+    "prot"              : ["prot%d" % n for n in range(1, 9)],
+    "rerun_success"     : ["rerun%02d" % int(n) for n in "1 2 3 4 5 6 7 8 17".split()],
+    "rerun_pf_error"    : ["rerun%02d" % int(n) for n in "9 10 11 12 13 16 20 21".split()],
+    "rerun_ana_error"   : ["rerun%02d" % int(n) for n in "14 15".split()],
+    "rerun_conf_error"  : ["rerun%02d" % int(n) for n in "18 19".split()],
+}
 
-DNA_names = ["DNA%d" % n for n in range(1, 9)]
-prot_names = ["prot%d" % n for n in range(1, 9)]
-rerun_success_names = ["rerun%02d" % int(n) for n in "1 2 3 4 5 6 7 8 17".split()]
-rerun_config_error = ["rerun%02d" % int(n) for n in "9 10 11 12 13 16".split()]
-rerun_analysis_error = ["rerun%02d" % int(n) for n in "14 15".split()]
-rerun_pf_error = ["rerun%02d" % int(n) for n in "18 19 20 21".split()]
 
 def pytest_generate_tests(metafunc):
     # This function feeds the output of the above function into the tests below
-    if "dna_folder" in metafunc.fixturenames:
-        metafunc.parametrize("dna_folder", DNA_names)
-    if "prot_folder" in metafunc.fixturenames:
-        metafunc.parametrize("prot_folder", prot_names)
-    if "rerun_success_folder" in metafunc.fixturenames:
-        metafunc.parametrize("rerun_success_folder", rerun_success_names)
-    if "rerun_pf_error_folder" in metafunc.fixturenames:
-        metafunc.parametrize("rerun_pf_error_folder", rerun_pf_error)
-    if "rerun_analysis_error_folder" in metafunc.fixturenames:
-        metafunc.parametrize("rerun_analysis_error_folder", rerun_analysis_error)
-    if "rerun_config_error_folder" in metafunc.fixturenames:
-        metafunc.parametrize("rerun_config_error_folder", rerun_config_error)
+    for test_type, folders in organise_tests.items():
+        if test_type in metafunc.fixturenames:
+            metafunc.parametrize(test_type, folders)
 
 
-def test_dna(dna_folder):
-    full_path = os.path.join(HERE, dna_folder)
+def test_dna(dna):
+    full_path = os.path.join(HERE, dna)
     main.call_main("DNA", "--compare %s" % full_path)
 
 
-def test_prot(prot_folder):
-    full_path = os.path.join(HERE, prot_folder)
+def test_prot(prot):
+    full_path = os.path.join(HERE, prot)
     main.call_main("protein", "--compare %s" % full_path)
 
 
@@ -44,28 +37,28 @@ def load_rerun(pth):
     dna3.extractall(pth)
 
 
-def test_rerun_success(rerun_success_folder):
-    full_path = os.path.join(HERE, rerun_success_folder)
+def test_rerun_success(rerun_success):
+    full_path = os.path.join(HERE, rerun_success)
     load_rerun(full_path)
     main.call_main("DNA", "%s" % full_path)
 
 
-def test_rerun_pf_error(rerun_pf_error_folder):
-    full_path = os.path.join(HERE, rerun_pf_error_folder)
+def test_rerun_pf_error(rerun_pf_error):
+    full_path = os.path.join(HERE, rerun_pf_error)
     load_rerun(full_path)
     with pytest.raises(util.PartitionFinderError):
         main.call_main("DNA", "%s" % full_path)
 
 
-def test_rerun_analysis_error(rerun_analysis_error_folder):
-    full_path = os.path.join(HERE, rerun_analysis_error_folder)
+def test_rerun_analysis_error(rerun_ana_error):
+    full_path = os.path.join(HERE, rerun_ana_error)
     load_rerun(full_path)
     with pytest.raises(analysis.AnalysisError):
         main.call_main("DNA", "%s" % full_path)
 
 
-def test_rerun_config_error(rerun_config_error_folder):
-    full_path = os.path.join(HERE, rerun_config_error_folder)
+def test_rerun_config_error(rerun_conf_error):
+    full_path = os.path.join(HERE, rerun_conf_error)
     load_rerun(full_path)
     with pytest.raises(config.ConfigurationError):
         main.call_main("DNA", "%s" % full_path)
