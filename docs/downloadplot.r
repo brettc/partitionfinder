2d1ee194f1b60858265e72115217dceba772f646
Rob Lanfear <rob.lanfear@gmail.com>
2013-08-01 08:29:57 +1000

    updated download stats and script

--- docs/downloadplot.r
+++ docs/downloadplot.r
@@ -4,11 +4,12 @@ library(reshape)
 dat <- read.csv("~/Documents/Projects_Current/partitionfinder/docs/downloadstats.csv")
 
 #the version changes in march and may need to be fixed up
+dat <- dat[,-c(4, 5)]
+
 dat <- aggregate(. ~ date, data=dat, FUN=sum)
 
 dat <- dat[order(as.Date(dat$date, format="%d/%m/%Y")),]
 
-dat <- dat[,-c(4)]
 
 dat$Mac <- cumsum(dat$Mac)
 dat$Windows <- cumsum(dat$Windows)
