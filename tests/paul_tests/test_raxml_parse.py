d0db7e623c106321441ff5703294cca761003110
Paul Frandsen <frandsen@eden.rutgers.edu>
2013-08-01 14:47:38 -0400

    test for raxml likelihood parser
    * contains a test for parsing likelihoods from the "RAxML_perSiteLLs"
    file

---
+++ tests/paul_tests/test_raxml_parse.py
@@ -0,0 +1,8 @@
+from raxml import likelihood_parser
+
+raxml_likelihood_file = "RAxML_perSiteLLs.testing"
+def test_likelihoods():
+    likelihood_list = [[-2.705940], [-2.705940], [-2.698479], [-5.225005], \
+        [-2.714398], [-2.698479], [-2.705940], [-2.705940], [-2.705940], \
+        [-5.255713]]
+    assert likelihood_parser(raxml_likelihood_file) == likelihood_list
