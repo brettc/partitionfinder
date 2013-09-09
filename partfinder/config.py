1741a2916bd5e92b31edbac02efe82fde3a3a04f
RobertLanfear <rob.lanfear@gmail.com>
2013-09-09 15:34:31 +1000

    removed print statement

--- partfinder/config.py
+++ partfinder/config.py
@@ -300,8 +300,6 @@ class Configuration(object):
                       " See the manual for more details.")
             raise ConfigurationError        
 
-        print option, value, self.phylogeny_program, self.kmeans_opt
-
         if option == "search" and "kmeans" in value and self.phylogeny_program != 'phyml' and self.kmeans_opt != 1:
             log.error("You have chosen a kmeans option (--kmeans_opt) that does not work "
                 "with the --raxml option. Please re-run your analysis in one of two ways: "
