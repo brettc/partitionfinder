fbd9b9abfba826968e1856af5fd2025a6f24f75c
RobertLanfear <rob.lanfear@gmail.com>
2013-10-17 10:19:03 +1100

    change plot size for download plots

--- docs/downloadplot.r
+++ docs/downloadplot.r
@@ -14,6 +14,7 @@ dl <- melt(dat[,c(1, 2, 3)])
 colnames(dl) <- c("date", "OS", "downloads")
 dl$date <- as.Date(dl$date, format = "%d/%m/%Y")
 
+quartz(width=10, height=5)
 p <- ggplot(dl, aes(date, downloads))
 p + geom_area(aes(colour = OS, fill= OS), position = 'stack')
 dev.copy2pdf(file="~/Documents/Projects_Current/partitionfinder/docs/downloadplot.pdf")
@@ -25,6 +26,7 @@ c <- dat[,c(1,5)]
 c$date <- as.Date(c$date, format = "%d/%m/%Y")
 c <- c[complete.cases(c),]
 
+quartz(width=10, height=5)
 p <- ggplot(c, aes(date, citations.googlescholar))
 p + geom_point() + geom_smooth(stat="identity")
 dev.copy2pdf(file="~/Documents/Projects_Current/partitionfinder/docs/citationsplot.pdf")
