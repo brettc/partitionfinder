f730ace4ebc85e7a5b26d5e6ec42068f0829d2bd
Rob Lanfear <rob.lanfear@gmail.com>
2013-11-21 15:10:59 +1100

    resovle subset.py conflice

--- partfinder/subset.py
+++ partfinder/subset.py
@@ -124,18 +124,7 @@ class Subset(object):
 
 
         if n < (K + 2):
-<<<<<<< HEAD
             log.debug(self.SMALL_WARNING % (self, n, model, K, self.name))
-=======
-            if cfg.model_selection.lower() == "aicc":
-                log.warning("The subset containing the following data_blocks: %s, has a very small"
-                        " number of sites (%d) compared to the number of parameters"
-                        " in the model being estimated (the %s model which has %d parameters)."
-                        " This may give misleading AICc results, so please check carefully"
-                        " if you are using the AICc for your analyses."
-                        " The model selection results for this subset are in the following file:"
-                        " /analysis/subsets/%s.txt\n" % (self, n, model, K, self.name))
->>>>>>> feature/morphology
             n = K + 2
 
         result.aicc = (-2.0 * lnL) + ((2.0 * K) * (n / (n - K - 1.0)))
