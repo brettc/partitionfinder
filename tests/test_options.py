a4d1408f003dda4842b156e89057325ac51e9189
Brett Calcott <brett.calcott@gmail.com>
2013-12-04 14:28:14 -0500

    remove unused test

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,5 +1,5 @@
-from partfinder import options, threadpool
-import shlex
+# from partfinder import options, threadpool
+# import shlex
 
 #def pytest_generate_tests(metafunc):
 #    # This function feeds the output of the above function into the tests below
@@ -10,14 +10,14 @@ import shlex
 #            ]
 #    )
 
-def test_processes():
-    o = options.Options()
-    o.parse_args(shlex.split('folder'))
-    assert o.processes == threadpool.get_cpu_count()
-    assert o.folder_path == 'folder'
-
-    o = options.Options()
-    o.parse_args(shlex.split('-p 3 folder'))
-    assert o.processes == 3
-
+# def test_processes():
+#     o = options.Options()
+#     o.parse_args(shlex.split('folder'))
+#     assert o.processes == threadpool.get_cpu_count()
+#     assert o.folder_path == 'folder'
+#
+#     o = options.Options()
+#     o.parse_args(shlex.split('-p 3 folder'))
+#     assert o.processes == 3
+#
 
