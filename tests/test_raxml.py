baf109473946f2a18d887972ca2189ac3a62f09b
Brett Calcott <brett.calcott@gmail.com>
2013-12-14 12:14:44 -0500

    fix Ramxl tests

--- tests/test_raxml.py
+++ tests/test_raxml.py
@@ -4,7 +4,11 @@ import tempfile
 from partfinder import raxml
 from partfinder.alignment import TestAlignment
 from partfinder import raxml_models
+from partfinder.config import Configuration
 
+def test_one():
+    # Just use the defaults
+    c = Configuration()
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 MISC_PATH = os.path.join(HERE, 'misc')
@@ -12,13 +16,15 @@ MISC_PATH = os.path.join(HERE, 'misc')
 
 def test_parse_nucleotide():
     pth = os.path.join(MISC_PATH, 'raxml_nucleotide.output')
-    p = raxml.Parser('DNA')
+    c = Configuration(datatype='DNA', phylogeny_program="raxml")
+    p = raxml.Parser(c)
     p.parse(open(pth).read())
 
 
 def test_parse_aminoacid():
     pth = os.path.join(MISC_PATH, 'raxml_aminoacid.output')
-    p = raxml.Parser('protein')
+    c = Configuration(datatype='protein', phylogeny_program="raxml")
+    p = raxml.Parser(c)
     p.parse(open(pth).read())
 
 
