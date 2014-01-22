92561b79c4cc9f981ea3bd5200123a653cca1071
april wright <wright.aprilm@gmail.com>
2014-01-22 14:32:51 -0600

    added rescaling as randint.py

--- randint.py
+++ randint.py
@@ -2,10 +2,17 @@ import unittest
 import numpy as np
 import random
 
+class ZeroException(Exception):
+    pass
+
 def make_mat():
 	a = np.random.randint(11, size=(5, 5))
 	print a
 	return a
+
+def read_in_array():
+	mat = np.genfromtxt("archy_mb.txt", skip_header=1, names=True)
+	print mat
 
 def rescale_mat():
 	a = make_mat()
@@ -13,22 +20,24 @@ def rescale_mat():
 	print a
 	return a
 
-
 def test_type_of():
+	"""Test to mkae sure matrix being imported is actually a numpy array, otherwise the next step does not work"""
 	a = make_mat()
 	assert isinstance(a, np.ndarray)
 	print 'Morphology data passes initial format check.'
 
 def test_lower_lim():
+	"""check that the rescaling worked: each column must begin from zero"""
 	a = rescale_mat()
 	b = a.min(axis = 0)
 	print b
 	for x in b:
-		assert (x == 0), "These values are not appropriately scaled, please check that all characters are numerical."
+		if (x != 0):
+			raise ZeroException("These values are not appropriately scaled, please check that your input matrix is comprised of numbers and not letters.")
+	print "Input matrix appropriately scaled; continuing to write out matrices."
 
 
-
-rescale_mat()
+test_lower_lim()
 
 
 
