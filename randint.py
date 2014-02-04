cedb014c9f867545832515e7cae5b8df872dd3bb
april wright <wright.aprilm@gmail.com>
2014-02-03 23:32:23 -0600

    added comments on timeit testing

--- randint.py
+++ randint.py
@@ -5,7 +5,7 @@ import scipy
 from scipy import stats
 
 def make_mat():
-	a = np.random.randint(5, size=(1000, 100000))
+	a = np.random.randint(5, size=(85, 300))
 	return a
 
 def read_in_array():
@@ -16,7 +16,6 @@ def rescale_mat():
 	"""RAxML expects symbols to be constant across columns, so we rescale the matrices to meet this reuirement"""
 	a = read_in_array()
 	a = np.ma.masked_array(a,np.isnan(a))
-#	a = np.array([[1, 2, 10],[1, 2, 99],[1,2,30]])
 #Solution with scipy: Times at 249ms per loop with 100 x 10000 matrix. 2.73s per loop for 1000 x 10000 and runs out of memory at 1000 x 10000.
 #	rescaled_matrix = scipy.stats.rankdata(a,'dense').reshape(a.shape)-1
 #Solution 2: This runs into a memory error on my computer with a 1000 x 100000 or 1000 x 10000 data set. Times at 410ms per loop with 100 x 10000 matrix. I think this is not our solution.
@@ -32,10 +31,11 @@ def fix_missing():
 	mat_list = rescale_mat()
 	a = mat_list[1]
 	a = a.tolist()
+#timeit solutions, including all the dependent functions: 1000x1000 size: 365ms per loop. 1000x10000: 3.67s per loop. 1000x100000: 36.8s per loop. Seems good.
 	a=[['?' if x is None else x for x in i] for i in a]
+	print a
 	return a
 	
-
 def test_type_of():
 	"""Test to mkae sure matrix being imported is actually a numpy array, otherwise the next step does not work"""
 	a = read_in_array()
@@ -45,7 +45,7 @@ def test_type_of():
 def test_rescaling():
 	"""check that the rescaling worked: the input matrix should not be the same as the rescaled, unless input contained zero"""
 	a = rescale_mat()
-#	print a[0], "\n", a[1]
+	print a[0], "\n", a[1]
 	rescaled = a[1]
 	try:
 		 np.array_equal(a[:1],a[1:])
@@ -53,7 +53,8 @@ def test_rescaling():
 		 print "Input matrix appropriately scaled; continuing to write out matrices."
 	except:
 		print "These values are not appropriately scaled, please check that your input matrix is comprised of numbers and not letters."
-	
+
+
 fix_missing()
 
 
