a9e55cfec8bc37d816e46f08be6e62254029fd69
Rob Lanfear <rob.lanfear@gmail.com>
2014-02-28 17:49:02 +1100

    update download stats, plots, and script

---
+++ docs/downloadplot (Autosaved).R
@@ -0,0 +1,64 @@
+library(ggplot2)
+library(reshape)
+library(zoo)
+
+dat <- read.csv("~/Documents/Projects_Current/partitionfinder/docs/downloadstats.csv")
+dat$date <- as.yearmon(dat$date)
+dat <- aggregate(. ~ date, data=dat, FUN=sum, na.action=na.pass)
+dat <- dat[order(as.Date(dat$date)),]
+
+dat$Mac <- cumsum(dat$Mac)
+dat$Windows <- cumsum(dat$Windows)
+ 
+
+# Download plot
+m <- dat[,c(1,2)]
+m$OS <- "Mac"
+names(m) <- c('date', 'downloads', 'OS')
+w <- dat[,c(1,3)]
+w$OS <- "Windows"
+names(w) <- c('date', 'downloads', 'OS')
+
+dl <- rbind(m, w)
+dl$date <- as.Date(dl$date, format = "%d/%m/%Y")
+
+quartz(width=10, height=5)
+p <- ggplot(dl, aes(date, downloads))
+p + geom_area(aes(colour = OS, fill= OS), position = 'stack')
+dev.copy2pdf(file="~/Documents/Projects_Current/partitionfinder/docs/downloadplot.pdf")
+dev.off()
+
+
+# Citation plot
+c <- dat[,c(1,5)]
+c$date <- as.Date(c$date, format = "%d/%m/%Y")
+c <- c[complete.cases(c),]
+
+quartz(width=10, height=5)
+p <- ggplot(c, aes(date, citations.googlescholar))
+p + geom_point() + geom_smooth(stat="identity")
+dev.copy2pdf(file="~/Documents/Projects_Current/partitionfinder/docs/citationsplot.pdf")
+dev.off()
+
+
+# Conversion rate plot (how many downloads per citation)
+d <- c[c(2:nrow(c)),]
+d$downloads <- m$downloads[19:length(m$downloads)] + m$downloads[19:length(m$downloads)]
+d$downloads.per.cite <- d$downloads / d$citations.googlescholar
+
+e <- d[,c(1,2)]
+e$type <- "citations"
+f <- d[,c(1,4)]
+f$type <- "downloads.per.citation"
+
+names(e) <- names(f) <- c("date", "value", "type")
+g <- rbind(e, f)
+
+quartz(width=10, height=5)
+p <- ggplot(g, aes(date, value, colour = type))
+p + geom_point() + geom_smooth(stat="identity")
+dev.copy2pdf(file="~/Documents/Projects_Current/partitionfinder/docs/citationsplot2.pdf")
+dev.off()
+
+
+
