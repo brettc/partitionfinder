a9090f4698f738a16c35a765f10d94e500ec947c
Brett Calcott <brett.calcott@gmail.com>
2015-02-24 16:06:50 +1100

    save tigger output in similar format to fast_TIGER

--- tigger.py
+++ tigger.py
@@ -1,11 +1,20 @@
 import sys
 from partfinder.alignment import Alignment
 from partfinder._tiger import TigerDNA
+from pathlib import Path
+import numpy
 
 if __name__ == "__main__":
     a = Alignment()
-    a.read(sys.argv[1])
+    filepath = Path(sys.argv[1])
+    a.read(str(filepath))
+
     tigger = TigerDNA()
     tigger.from_alignment(a)
-    print tigger.calc_rates()
+    rates = tigger.calc_rates()
+    output_path = str(filepath.with_suffix('.tigger8'))
+    numpy.savetxt(output_path, rates, fmt="%5f", delimiter='\n')
+
+
+    
 
