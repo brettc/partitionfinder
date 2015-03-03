ea2566d713292fb070cf12056f3f9e053916e170
RobLanf <rob.lanfear@gmail.com>
2015-03-04 08:07:23 +1100

    deleted cuff from model_loader.py

--- partfinder/model_loader.py
+++ partfinder/model_loader.py
@@ -60,6 +60,8 @@ def get_available_models(all_models, the_config):
         available_models = available_models.query("datatype=='protein'")
     elif the_config.datatype == 'morphology':
         available_models = available_models.query("datatype=='morphology'")
+    else:
+        log.error("Unknown datatype '%s'" % the_config.datatype)
 
     if len(available_models) == 0:
         log.error("""Phylogeny program '%s' does not implement any models that deal 
@@ -150,42 +152,6 @@ def expand_model_list(the_config):
         raise PartitionFinderError
 
 
-def get_mrbayes_models():
-    """
-    Return a list of all models implemented in MrBayes. Thanks to Ainsley Seago
-    for this.
-    """
-    mrbayes_base_models = ["JC", "F81", "K80", "HKY", "SYM", "GTR"]
-    model_list = []
-    for model in mrbayes_base_models:
-        model_list.append(model)
-        model_list.append("%s+I" % model)
-        model_list.append("%s+G" % model)
-        model_list.append("%s+I+G" % model)
-    return model_list
-
-def get_beast_models():
-    """
-    Return a list of all models implemented in BEAST v1.7.2.
-    """
-    beast_base_models = ["K80", "TrNef", "SYM", "HKY", "TrN", "GTR"]
-    model_list = []
-    for model in beast_base_models:
-        model_list.append(model)
-        model_list.append("%s+I" % model)
-        model_list.append("%s+G" % model)
-        model_list.append("%s+I+G" % model)
-    return model_list
-
-def get_raxml_models():
-    """
-    Return a list of all models implemented in RaxML. Thanks to Ainsley Seago
-    for this.
-    """
-    model_list = ["GTR+G", "GTR+I+G"]
-    return model_list
-
-
 def check_for_duplicates(models):
     # model lists shouldn't contain duplicated models
     duplicates = [x for x, y in collections.Counter(models).items() if y > 1]
@@ -194,116 +160,3 @@ def check_for_duplicates(models):
                   the following models seem to be duplicated: %s"""
                   % duplicates)
         raise PartitionFinderError
-
-
-def set_models(self, text, loc, tokens):
-    # TODO: Fix this ugly mess.
-    if self.cfg.phylogeny_program == "phyml":
-        self.phylo_models = phyml_models
-    elif self.cfg.phylogeny_program == "raxml":
-        self.phylo_models = raxml_models
-
-    all_dna_mods = set(self.phylo_models.get_all_dna_models())
-    all_protein_mods = set(self.phylo_models.get_all_protein_models())
-    total_mods = all_dna_mods | all_protein_mods
-
-    mods = tokens[1]
-    DNA_mods = 0
-    protein_mods = 0
-    if mods.userlist:
-        modlist = mods.userlist
-        log.info("Setting 'models' to a user-specified list")
-    else:
-        modsgroup = mods.predefined
-        if modsgroup.lower() == "all":
-            modlist = list(all_dna_mods)
-            DNA_mods += 1
-        elif modsgroup.lower() == "mrbayes":
-            modlist = set(phyml_models.get_mrbayes_models())
-            DNA_mods += 1
-        elif modsgroup.lower() == "beast":
-            modlist = set(phyml_models.get_beast_models())
-            DNA_mods += 1
-        elif modsgroup.lower() == "raxml":
-            modlist = set(phyml_models.get_raxml_models())
-            DNA_mods += 1
-        elif modsgroup.lower() == "all_protein":
-            modlist = set(self.phylo_models.get_all_protein_models())
-            protein_mods += 1
-        elif modsgroup.lower() == "all_protein_gamma":
-            if self.cfg.phylogeny_program == "raxml":
-                modlist = set(raxml_models.get_protein_models_gamma())
-                protein_mods += 1
-            else:
-                log.error(
-                    """The models option 'all_protein_gamma' is only
-                    available with raxml  (the --raxml commandline option).
-                    Please check and try again""")
-                raise ParserError
-        elif modsgroup.lower() == "all_protein_gammaI":
-            if self.cfg.phylogeny_program == "raxml":
-                modlist = set(raxml_models.get_protein_models_gammaI())
-                protein_mods += 1
-            else:
-                log.error(
-                    """The models option 'all_protein_gammaI' is only
-                    available with raxml (the --raxml commandline option).
-                    Please check and try again""")
-                raise ParserError
-        else:
-            pass
-
-        # never include the LG4X model in predefined model lists
-        # because it can't (yet) be used for partitionined analyses
-        modlist = filter(lambda x: x.count("LG4X")==0, modlist)
-
-
-        log.info("Setting 'models' to '%s'" % modsgroup)
-
-    self.cfg.models = set()
-    for m in modlist:
-        if m not in total_mods:
-            raise ParserError(
-                text, loc, "'%s' is not a valid model for phylogeny "
-                           "program %s. Please check the lists of valid models in the"
-                           " manual and try again" % (m, self.cfg.phylogeny_program))
-
-        if m in all_dna_mods:
-            DNA_mods += 1
-        if m in all_protein_mods:
-            protein_mods += 1
-
-        self.cfg.models.add(m)
-
-    log.info("The models included in this analysis are: %s",
-             ", ".join(self.cfg.models))
-    self.cfg.model_count = len(self.cfg.models)
-
-    # Check data type against the model list that we've got a sensible
-    # model list
-    if DNA_mods > 0 and protein_mods == 0 and self.cfg.datatype == "DNA":
-        log.info("Setting datatype to 'DNA'")
-    elif DNA_mods == 0 and protein_mods > 0 and self.cfg.datatype == "protein":
-        log.info("Setting datatype to 'protein'")
-    elif DNA_mods == 0 and protein_mods > 0 and self.cfg.datatype == "DNA":
-        raise ParserError(
-            text, loc, "The models list contains only models of amino acid change."
-                       " PartitionFinder.py only works with nucleotide models (like the GTR model)."
-                       " If you're analysing an amino acid dataset, please use PartitionFinderProtein,"
-                       " which you can download here: www.robertlanfear.com/partitionfinder."
-                       " The models line in the .cfg file is")
-    elif DNA_mods > 0 and protein_mods == 0 and self.cfg.datatype == "protein":
-        raise ParserError(
-            text, loc, "The models list contains only models of nucleotide change."
-                       " PartitionFinderProtein.py only works with amino acid models (like the WAG model)."
-                       " If you're analysing a nucleotide dataset, please use PartitionFinder.py,"
-                       " which you can download here: www.robertlanfear.com/partitionfinder"
-                       " The models line in the .cfg file is")
-    else:  # we've got a mixture of models.
-        raise ParserError(
-            text, loc, "The models list contains a mixture of protein and nucleotide models."
-                       " If you're analysing a nucleotide dataset, please use PartitionFinder."
-                       " If you're analysing an amino acid dataset, please use PartitionFinderProtein."
-                       " You can download both of these programs from here: www.robertlanfear.com/partitionfinder"
-                       " The models line in the .cfg file is")
-# cut down models to only those in the DNA/Protein and PhyML/RAxML models
