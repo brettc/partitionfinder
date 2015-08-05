909267f6f7e59d5920532a4f01e49f896f9bad4c
Brett Calcott <brett.calcott@gmail.com>
2015-08-05 19:06:45 +1200

    Add phyml as submodule and setup build-programs to build it

--- build-programs.sh
+++ build-programs.sh
@@ -1,2 +1,21 @@
-cd programs/phyml_source/
-./waf configure build install
+# Get the submodules
+git submodule init
+git submodule update
+
+# Building Phyml
+# This is modified from the confphy...
+# It assumes that you've done:
+# brew install automake autoconf libtool 
+cd submodules/phyml
+aclocal
+autoheader
+autoconf -f
+# Note: brew renames this...
+glibtoolize
+automake -f --add-missing
+./configure
+make clean
+make
+
+mv -f ./submodules/phyml/src/phyml ./programs
+
