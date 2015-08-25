8bd784c786afa8ac218446e991397515de2b6c07
RobertLanfear <rob.lanfear@gmail.com>
2015-08-25 15:17:09 +1000

    changed user output for cluster

--- partfinder/neighbour.py
+++ partfinder/neighbour.py
@@ -196,7 +196,7 @@ def update_c_matrix(c_matrix, sub_tuples, subsets, diffs):
 
 def get_best_pair(c_matrix, best_change, subsets):
 
-    log.info("C matrix: %s", str(c_matrix))
+    log.debug("C matrix: %s", str(c_matrix))
 
     if len(c_matrix.shape) == 1:
         log.debug("C matrix shape was == 1")
@@ -206,7 +206,7 @@ def get_best_pair(c_matrix, best_change, subsets):
     l = np.where(c_matrix==best_change)
 
 
-    log.info("Location of best_change in c_matrix: %s", str(l))
+    log.debug("Location of best_change in c_matrix: %s", str(l))
 
     # this function is only called if the best_change is <0
     # so we can be sure that we are on an off-diagonal
