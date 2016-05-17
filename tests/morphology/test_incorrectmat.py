35863b2a1dd46e84b07f811841595534a83f70da
wrightaprilm <wright.aprilm@gmail.com>
2016-05-16 20:54:06 -0500

    fixed one test.

--- tests/morphology/test_incorrectmat.py
+++ tests/morphology/test_incorrectmat.py
@@ -1,22 +1,12 @@
 import pytest
-from partfinder import alignment
+import os
+from partfinder import main, util, scheme, analysis_method, config, alignment, results, reporter
 
 
-def test_broken():
-    '''This test should fail due to missing character in line 5.'''
-    test = """
-10 2
-Allosaurus_fragilis                 11
-Sinraptor                           11
-Dilong_paradoxus                    11
-Eotyrannus_lengi                    11
-Tyrannosaurus_rex                   1 
-Gorgosaurus_libratus                11
-Tanycolagreus_topwilsoni            11
-Coelurus_fragilis                   11
-Ornitholestes_hermanni              10
-Huaxiagnathus_orientalis            10
-    """
-    alignment.AlignmentParser(test)		
+def test_incmat():
+	'''This test should fail due to incorrect line length'''
+	HERE = os.path.abspath(os.path.dirname(__file__))
+	full_path = os.path.join(HERE, "incmat")
+	main.call_main("morphology", '--min-subset-size 1 --raxml "%s"' % full_path)	
 
     
